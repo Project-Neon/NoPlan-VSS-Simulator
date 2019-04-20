ea88bd3a5ccd1334ca014a0d16bf58de8b430d4b
Alexsandro Francisco <alexsandro2134@gmail.com>
2019-04-20 14:54:03 -0300

    Add env var to choose n. of team

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import sys
 from src.kernel import Kernel
 
 def main():
-    kernel = Kernel(True)
-    kernel.loop()
+
+	if len(sys.argv) > 1:
+		two_teams = int(sys.argv[1])
+	else:
+		two_teams = 0
+
+	kernel = Kernel(two_teams)
+	kernel.loop()
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main()
+    main()	
